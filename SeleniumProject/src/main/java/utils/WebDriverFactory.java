b7bd9f6b5866d513b2adb7158180a30a5b353736
Zul Qarnain <odwltdael@gmail.com>
2026-03-11 04:01:10 +0530

    Updating WebDriver

--- SeleniumProject/src/main/java/utils/WebDriverFactory.java
+++ SeleniumProject/src/main/java/utils/WebDriverFactory.java
@@ -2,14 +2,22 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebDriverFactory {
     public static WebDriver getDriver() {
-    	System.setProperty("webdriver.edge.driver", "driver\\msedgedriver.exe");
+//    	System.setProperty("webdriver.edge.driver", "driver\\msedgedriver.exe");
 //        driver = new EdgeDriver();
-        return new EdgeDriver();
+       
+    	WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        return new ChromeDriver(options);
     }
 }
